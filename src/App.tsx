5d39ea8351b5482b7116b1efa43fcabf95507d12
jieun419 <crg1050@naver.com>
2023-11-02 22:27:18 +0900

    feat: 포켓몬 이름 보여주기

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,45 @@
+import { useQuery } from "react-query"
+import axios from "axios"
+import { useState } from "react"
+
+interface PokeDataT {
+  name: string,
+  url: string,
+}
+
 function App() {
+  const [pokeData, setPokeData] = useState<PokeDataT[]>([])
+  const getPokeData = async () => {
+    const res = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
+    return res.data
+  }
+
+  useQuery({
+    queryKey: ["pokeData"],
+    queryFn: () => getPokeData(),
+    onSuccess(data) {
+      setPokeData(data.results)
+    },
+    onError(err) {
+      console.log(err);
+    },
+  })
+
+  console.log(pokeData)
 
   return (
     <>
-      얍!
+      <div>
+        {
+          pokeData.map((el) => (
+            <div key={el.name}>
+              <p>
+                {el.name}
+              </p>
+            </div>
+          ))
+        }
+      </div>
     </>
   )
 }
